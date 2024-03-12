23d94152b290c06d0d0c937686230f014dd07436
Njood-917 <njood9177@gmail.com>
2024-03-12 22:38:54 +0300

    fifth Assignment 5_4

---
+++ src/test/java/junit/Ass5_4.java
@@ -0,0 +1,28 @@
+package junit;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Cookie;
+
+import java.util.Set;
+
+/*   Go to URL: http://facebook.com
+       getCookies,
+       addCookie,
+       deleteCookieNamed,
+       deleteAllCookies
+    */
+public class Ass5_4 extends Tests{
+    @Test
+    public void cookieTest(){
+        driver.navigate().to("http://facebook.com");
+        // Getting all cookies
+        System.out.println("All Cookie: " + driver.manage().getCookies());
+
+        Cookie addCookie = new Cookie("Njood" ,"saad");
+        driver.manage().addCookie(addCookie);
+        driver.manage().deleteCookieNamed("Njood");
+        driver.manage().deleteAllCookies();
+
+
+    }
+}
